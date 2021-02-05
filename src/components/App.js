2ff8fe0a27d94fb0380e1bd90b4f6626e88c61df
Kryshlynn Florante <kryshflorante@gmail.com>
2021-02-05 14:59:31 +0100

    make a form with an input and label on the new SearchBar component with some added semantic-ui styles

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,9 @@ import SearchBar from './SearchBar';
 class App extends React.Component{
   render() {
     return (
-      <SearchBar />
+      <div className="ui container">
+        <SearchBar /> 
+      </div>
     );
   }
 }
