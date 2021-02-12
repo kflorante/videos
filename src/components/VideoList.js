fe62086631d64844b482f266d3f8c997fa8df469
Kryshlynn Florante <kryshflorante@gmail.com>
2021-02-12 16:42:05 +0100

    pass down the video prop to each VideoItem to show the title and thumbnail of each search result

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,7 +4,7 @@ import VideoItem from './VideoItem';
 const VideoList = ({ videos  }) => {
   const renderVideoItems = videos.map(video => {
     return (
-      <VideoItem />
+      <VideoItem video={video} />
     );
   });
   return (
