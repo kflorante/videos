65e89ac78a55676b20124b7657bcf3758e1682a4
Kryshlynn Florante <kryshflorante@gmail.com>
2021-02-16 18:09:11 +0100

    add a new parameter in calling the youtube api to only search for video types and not include playlists

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -6,6 +6,7 @@ const KEY = 'AIzaSyCyZm0anF2yGIwMUpiCFDTaCcFQYjGhdig';
    baseURL: 'https://www.googleapis.com/youtube/v3',
    params: {
      part: 'snippet',
+     type: 'video',
      maxResults: 5,
      key: KEY
    }
