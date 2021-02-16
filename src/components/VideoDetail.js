51ba3be9d5e615f7789b2b208854b8ba5577872b
Kryshlynn Florante <kryshflorante@gmail.com>
2021-02-16 17:20:15 +0100

    embed the video being clicked using iframe tag

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -6,10 +6,18 @@ const VideoDetail = ({ video }) => {
       <div>Loading...</div>
     );
   }
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return (
-    <div className="ui segment">
-      <h4 className="ui header">{video.snippet.title}</h4>
-      <p>{video.snippet.description}</p>
+    <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} />
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
     </div>
   );
 };
