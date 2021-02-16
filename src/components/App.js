136f6ff91d54a3d28ce6184bf9558e8564d294c4
Kryshlynn Florante <kryshflorante@gmail.com>
2021-02-16 18:06:31 +0100

    assign a default search term so that the page already loads with a playable video right away

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,10 @@ class App extends React.Component{
     selectedVideo: null
   };
 
+  // componentDidMount() {
+  //   this.onTermSubmit('karaoke');
+  // }
+
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
       params: {
